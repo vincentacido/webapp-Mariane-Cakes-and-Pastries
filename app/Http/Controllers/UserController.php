926d7e5d1e8f27c571e489eaab23c75c632658e4
vincentacido <acidovincent6@gmail.com>
2023-01-20 06:27:27 -0800

    Added Functionality on Dashboard and User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('users.users', [
+            'header'    => 'Users Management',
+            'users'     => User::all()
+        ]);
+    }
+
+    public function form()
+    {
+        return view('users.form', [
+            'header'    => 'Add User'
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', Rules\Password::defaults()],
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        session()->flash('status', 'Added User Successfully!');
+
+        return redirect('/users');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/users/users.blade.php
@@ -0,0 +1,61 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        {{-- <div class="max-w-7xl mx-auto sm:px-6 lg:my-5">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            bhgh
+            </div>
+        </div> --}}
+
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <h2 class="float-left">
+                        {{ $header }}
+                    </h2>
+
+                    <a href="{{ url('/users/add') }}">
+                        <button class="float-right rounded-full bg-pink-500 p-1 hover:bg-sky-700">
+                            Add Users
+                        </button>
+                    </a>
+                
+                
+                    <table class="table-auto w-full">
+                         <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Action</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td> {{ $user->name }} </td>
+                                    <td class="text-center"> {{ $user->email }} </td>
+                                    <td class="text-center">
+                                        <button class="rounded-full bg-sky-500 p-1 hover:bg-sky-700">Update</button>
+                                        <button class="rounded-full bg-sky-500 p-1 hover:bg-sky-700">Delete</button>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>              
