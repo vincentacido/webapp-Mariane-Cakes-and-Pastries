030792abed6c4c67156344a573e99e8c0fa5325e
vincentacido <acidovincent6@gmail.com>
2023-01-24 01:38:36 -0800

    Edited Homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -312,14 +312,14 @@
                             <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
                         </div>
                     </div>
-                    <!-- Submit error message-->
+                    {{-- <!-- Submit error message-->
                     <!---->
                     <!-- This is what your users will see when there is-->
                     <!-- an error submitting the form-->
                     <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
                     <!-- Submit Button-->
                     <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Send Message</button></div>
-                </form>
+                </form> --}}
             </div>
         </section>
         
